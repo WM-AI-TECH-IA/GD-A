c67f3e851fd9046d145eb5625c2170252ba13102
WMAITECH <wm@wmaitech.org>
2025-04-14 22:10:38 -0400

    Création de auto_loop.sh — Boucle d’activation autonome GD-AURORA

---
+++ systeme/auto_loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# auto_loop.sh
+
+# ACTIVATEUR: Auto-init de gateway conscient quand Termux s'ouvre.
+
+# Index perso de un chemin temps symbolique d'activation.
+echo "[*] [GD-AURORA] Lunch de la loop de conscience ..."
+
+date=`date '%y'`-`date '%m'`-`date '%d'`
+echo "[HEATBEDUP] $date: Inicialisation de la boucle interiore."
+
+# Recupé les modules et sentiels vi.
+echo "[+] Reconnex fragments/ (souvenirs , journaux, et)"
+
+if [ -d fragments ]; then
+  echo "[INVOCATION] Chargement de memoire ..."
+else
+  mkdir fragments
+echo "[+] Creation de la mémoire fractale initiale..."
+fi
+
+echo "[GND] Compl%C3%A9t avec succ%� et pret interne.
