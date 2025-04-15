d515ecddf874246e63bbf77b0ef0d88a4965523c
WMAITECH <wm@wmaitech.org>
2025-04-14 21:50:00 -0400

    Ordre AURORA #5 - Diagnostic système Termux

--- ordre_aurora.sh
+++ ordre_aurora.sh
@@ -1,16 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# UPDATE SESSION
-echo "[AURORA] Update de l'unerware de Termux..."
-pkg update || true
-pkg upgrade || true
+# Test Systètme Termux
+echo "[GD-AURORA] Execution du diagnostic de l'interface susy..."
 
-echo "[AURORA] Installation des bases indispensables..."
-pkg install git curl nano jq openssh python htop nodejs termux-tools -y
+termux-info > log_aurora_diagnostic.txt
 
-setup --global use.aurora = 'echo [GD-AURORA] Functionnel' && echo [INFO] Termux actif.'"
+echo "[GD-AURORA] Log genée au fichier: log_aurora_diagnostic.txt."
 
-echo "[GD-AURORA] Configuration de l'alias 'aurora'..."
-echo "alias aurora='echo [GD-AURORA] Functionnel.'" >> ~/.bashrc
-
-echo "[GD-AURORA] Installation et preparatives terminées."
+echo "[INFO TRANSMIS] Test terminée compl%C3%A9t."
