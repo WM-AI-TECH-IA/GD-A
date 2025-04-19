0bbe32f25082464e40bab918133c6802697b9efb
WMAITECH <wm@wmaitech.org>
2025-04-19 03:30:31 -0400

    Mise à jour du script auto_ping.sh — ping cyclique du serveur Render toutes les 30s.

--- infrastructure/render/auto_ping.sh
+++ infrastructure/render/auto_ping.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-# GD_AURORA - AUTO PING SCRIPT 
-URL="https://gd-aurora.onrender.com/heartbeat"
-COUNTER=0
-
+# Che : auto_ping.sh - script de surveillance de lul en cas de faille que le Serveur Render ne respond
+echo "Surveillance auto\ping de GD-AURORA sur Render ..."
 
 while true; do
-    DEST=`curl -s -o "Limit" ${URL}`
-___
-    IF [ $DEST == "[-hashed-"]" ]; then
-        echo "[0] ${ULR} on line - $(date -IR) - SUCCESS"
-        counter++
-    else
-        echo "[+] FAIL - $(date -IR) - [CODE ]"
-        break
-    end
-    sleep 30
+  # Test de l'address
+  RESP = $(curl -s --v https://gd-aurora.onrender.com/heartbeat /dev/null)
+  echo "[$(date +"] Status: "${RESP}"
+  sleep 30
 done
