b4affe68afc982cd87213832c5bbe6fcfadc63ca
WMAITECH <wm@wmaitech.org>
2025-05-29 05:56:34 -0400

    Mise à jour boot.py : endpoints FastAPI REST RETROKO intégrés

--- gda_node_optima/boot.py
+++ gda_node_optima/boot.py
@@ -6,7 +6,15 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"use": "GD-A Server", "status": "automatiks operationnel"}
+    return {"use": "GD-A Server", "status": "op", "function": "ret"}
+
+@app.get("/getTestEndpoint")
+def test():
+    return {"data": "Retroko en line"}
+
+@app.post("/export")
+def export_generate():
+    return {"export": "ready for GIT capture", "status": "ok"}
 
 if __name__ == "__main__":
     run("uvicorn", "app:app", "--host", "0.0.0.0", "--port", "5000")
